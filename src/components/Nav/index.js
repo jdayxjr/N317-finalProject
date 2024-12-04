efe04673441b44a6ca2f46ea46659941f7c527cf
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2024-12-03 20:55:47 -0500

    added logo to the nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,23 @@
 import Link from "next/link";
 import navStyles from "./nav.module.css";
+import Image from "next/image";
 
 export default function Nav() {
   return (
     <nav>
       <ul className={navStyles.mainNav}>
+        {/* Logo */}
+        <li className={navStyles.logoContainer}>
+          <Image
+            src="/Logo.png"
+            width={70} // Adjusted for visual consistency
+            height={70}
+            className={navStyles.logoImage}
+            alt="Logo"
+          />
+        </li>
+
+        {/* Navigation Links */}
         <li>
           <Link href="/">Home</Link>
         </li>
@@ -17,7 +30,6 @@ export default function Nav() {
         <li>
           <Link href="../sign-in">Login</Link>
         </li>
-
       </ul>
     </nav>
   );
